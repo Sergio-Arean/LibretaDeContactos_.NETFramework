151049116dbd234aa0e6e4a045dcfc75d5c957e6
Sergio Arean <101679332+Sergio-Arean@users.noreply.github.com>
2023-07-11 12:46:12 -0300

    Add files via upload

---
+++ Modelo/Contacto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    public class Contacto : IComparable<Contacto>
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+
+        public string Telefono { get; set; }
+
+        public string Email { get; set; }
+
+        public string ImagenUrl { get; set; }
+
+        public int CompareTo(Contacto other)
+        {
+            if(other is Contacto)
+            {
+                return string.Compare(this.Nombre, other.Nombre);
+            }
+            else
+            {
+                throw new Exception();
+            }
+        }
+    }
+}
